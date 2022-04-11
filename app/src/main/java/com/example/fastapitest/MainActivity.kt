a0ed855208f88ca6256dc6ba66937132fdf1ca91
yo <ya90312@gmail.com>
2022-04-11 23:44:59 +0800

    切換至查詢頁面功能

--- app/src/main/java/com/example/fastapitest/MainActivity.kt
+++ app/src/main/java/com/example/fastapitest/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.fastapitest
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -9,7 +10,7 @@ import okhttp3.*
 import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
-    private val url = "https://cb67-2001-b011-b800-bc8c-d478-4999-d69-ae55.ngrok.io/"
+    private val url = "https://39cd-2001-b011-b800-bd53-6d06-ab4f-2fbf-f471.ngrok.io/"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -29,6 +30,11 @@ class MainActivity : AppCompatActivity() {
 
             })
 
+        }
+        btn_search_page.setOnClickListener {
+            // 切換頁面
+            val search_page = Intent(this,MedicineSearchActivity::class.java)
+            startActivity(search_page)
         }
     }
 }
